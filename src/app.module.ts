59bd4ffd7f416bd6525cd5a50067ccf5f020034e
Ahmed Hany <ah7608867@gmail.com>
2024-12-05 19:57:33 +0200

    project setup

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReportModule } from './report/report.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [ReportModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
