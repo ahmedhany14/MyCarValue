239474a4673c53b67e4797974dfb9195a815e502
Ahmed Hany <ah7608867@gmail.com>
2024-12-14 16:03:50 +0200

    configration files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportModule } from './report/report.module';
 import { UserModule } from './user/user.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entitie/user.entitie';
 import { Report } from './report/entitie/report.entitie';
@@ -12,12 +12,26 @@ import { join } from 'path';
 @Module({
 
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get('DB_NAME'),
+        entities: [User, Report],
+        synchronize: true,
+      }),
+    }),
+    /*
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: join(__dirname, '..', 'db.sqlite'), // Use absolute path
       entities: [User, Report],
       synchronize: true,
-    }),
+    }),*/
     ReportModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
